03bbb14bf746d13bece9c4440c3c574d540e171c
unknown <till.a@freenet.de>
2015-04-20 14:01:09 +0200

    refactoring und program counter

--- src/storage.java
+++ src/storage.java
@@ -1,9 +1,12 @@
 
 public class storage
 {
+	public logic log;
+	public GUI gui;
+	public control ctrl;
+	
 	public int[] progStorage = new int[1024];		//Array für 14 bit Programmspeicher
 	public int[] dataStorage = new  int[256];		//Array für 8 bit Datenspeicher
-	public int pc = 0; 								//Programmzählert
 	
 	storage()
 	{
@@ -11,5 +14,35 @@ public class storage
 		//dataStorage[10] = 255;
 	}
 	
+	/**
+	 * Getters
+	 */
+	int getPC() {
+		return dataStorage[0x82];
+	}
 	
+	/**
+	 * Setters
+	 */
+	
+	void setPC(int value) {
+		dataStorage[0x82] = value;
+	}
+	
+	void setPCLath(int value) {
+		System.out.println(dataStorage[0x0A]);
+		System.out.println(dataStorage[0x0A] & 00011111);
+		System.out.println((value << 5));
+		
+		dataStorage[0x0A] = (dataStorage[0x0A] & 00011111) | (value << 5);
+		
+	}
+	
+	/**
+	 * Other methods
+	 */
+	
+	void increasePC() {
+		dataStorage[0x82]++;
+	}
 }
